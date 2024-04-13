80fbd7a77676dff212341a5dbac3b450fef9529d
Fernando <55415857+fernando-mendoza@users.noreply.github.com>
2024-04-13 12:03:58 -0600

    feat: adding users command
    print only users from the data json

--- cli.py
+++ cli.py
@@ -1,9 +1,15 @@
 import click
+import utils
 
 @click.group()
 def cli():
     pass
 
+@cli.command()
+def users():
+    users = utils.read_json()
+    for user in users:
+        print(f"{user['id']} - {user['name']} - {user['lastname']}")
 
 if __name__ == '__main__':
     cli()
