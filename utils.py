ae802fe6ac45deb15af1638648d589716de9d3b6
Fernando <55415857+fernando-mendoza@users.noreply.github.com>
2024-04-13 12:14:28 -0600

    feat: adding write_json func in utils

--- utils.py
+++ utils.py
@@ -8,3 +8,7 @@ def read_json():
     with open('data.json', 'r') as f:
         data = json.load(f)
     return data
+
+def write_json(data):
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
